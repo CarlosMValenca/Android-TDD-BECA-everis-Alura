f4e1335be66730c39ef3f7a05c6144606f04486e
Carlos M Valenca <cvalenca@everis.com>
2021-10-15 15:59:01 -0300

    Refatorando os testes

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 
 public class LeilaoTest {
 
+    private Leilao console = new Leilao("Console");
+    private Usuario alex = new Usuario("Alex");
+    private Usuario fran = new Usuario("Fran");
+
     @Test
     public void getDescricaoQuandoRecebeDescricaoDevolveDescricao() {
-        // criar cenário de teste
-        Leilao console =  new Leilao("Console");
         // executar ação esperada
         String descrcaoDevolvida = console.getDescricao();
         // testar resultado esperado
@@ -17,40 +19,64 @@ public class LeilaoTest {
     }
 
     // [nome do metodo] + [estado do teste] + [resultado esperado]
+    // [deve]+[resultado esperado]+[estado de teste]
     @Test
-    public void getMaiorLanceQuandoREcebeApenasUmLanceDevolvendoMaiorLance(){
+    public void deve_DevolvendoMaiorLance_QuandoRecebeApenasUmLance(){
         // verifica se devolve maior lance com apenas um lance
-        Leilao console = new Leilao("Console");
-        console.propoe(new Lance(new Usuario("Alex"), 200.00));
+        console.propoe(new Lance(alex, 200.00));
 
         double maiorLanceDevolvidoDoConsole = console.getMaiorLance();
         // O delta pega a diferença entre os valores com ponto flutuante e se ele for maior, significa que os valores são equivalentes.
         assertEquals(200.00, maiorLanceDevolvidoDoConsole, 0.0001);
-
     }
 
     @Test
-    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance(){
+    public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente(){
         // verifica se devolve maior lance com mais de um lance em ordem crescente
-        Leilao computador = new Leilao("Computador");
-        computador.propoe(new Lance(new Usuario("Alex"), 100.00));
-        computador.propoe(new Lance(new Usuario("Fran"), 200.00));
+        console.propoe(new Lance(alex, 100.00));
+        console.propoe(new Lance(fran, 200.00));
 
-        double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
+        double maiorLanceDevolvidoDoComputador = console.getMaiorLance();
 
         assertEquals(200.00, maiorLanceDevolvidoDoComputador, 0.0001);
-
     }
 
     @Test
-    public void getMaiorLanceLanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescenteDevolveMaiorLance(){
+    public void deve_DevolveMaiorLance_LanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescente(){
         // verifica se devolve maior lance com mais de um lance em ordem decrescente
-        Leilao carro = new Leilao("Carro");
-        carro.propoe(new Lance(new Usuario("Alex"), 10000.00));
-        carro.propoe(new Lance(new Usuario("Fran"), 9000.00));
+        console.propoe(new Lance(alex, 10000.00));
+        console.propoe(new Lance(fran, 9000.00));
 
-        double maiorLanceDevolvidoDoCarro = carro.getMaiorLance();
+        double maiorLanceDevolvidoDoCarro = console.getMaiorLance();
 
         assertEquals(10000.00, maiorLanceDevolvidoDoCarro, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolvendoMenorLance_QuandoRecebeApenasUmLance(){
+        console.propoe(new Lance(alex, 200.00));
+
+        double menorLanceDevolvido = console.getMenorLance();
+        assertEquals(200.00, menorLanceDevolvido, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente(){
+        console.propoe(new Lance(alex, 100.00));
+        console.propoe(new Lance(fran, 200.00));
+
+        double menorLanceDevolvidoDoComputador = console.getMenorLance();
+
+        assertEquals(100.00, menorLanceDevolvidoDoComputador, 0.0001);
+    }
+
+    @Test
+    public void deve_DevolveMenorLance_LanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescente(){
+        console.propoe(new Lance(alex, 10000.00));
+        console.propoe(new Lance(fran, 9000.00));
+
+        double menorLanceDevolvidoDoCarro = console.getMenorLance();
+
+        assertEquals(9000.00, menorLanceDevolvidoDoCarro, 0.0001);
     }
 }
