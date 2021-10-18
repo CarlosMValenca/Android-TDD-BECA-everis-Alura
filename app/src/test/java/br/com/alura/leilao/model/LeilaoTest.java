8062179eb14bddf011e09ae79baf2489bc24f48a
Carlos M Valenca <cvalenca@everis.com>
2021-10-18 09:46:11 -0300

    Mais de cinco lances

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -127,16 +127,25 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolverTresLances_QuandoReceberApenasQuatroLances(){
+    public void deve_DevolverTresLances_QuandoReceberMaisDeTresLances(){
         CONSOLE.propoe(new Lance(ALEX, 300.00));
         CONSOLE.propoe(new Lance(FRAN, 400.00));
         CONSOLE.propoe(new Lance(ALEX, 500.00));
         CONSOLE.propoe(new Lance(FRAN, 600.00));
 
-        final List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
-        assertEquals(3, tresMaioresLancesDevolvidos.size());
-        assertEquals(600.00, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
-        assertEquals(500.00, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
-        assertEquals(400.00, tresMaioresLancesDevolvidos.get(2).getValor(), DELTA);
+        final List<Lance> tresMaioresLancesDevolvidosParaQuatroLances = CONSOLE.tresMaioresLances();
+        assertEquals(3, tresMaioresLancesDevolvidosParaQuatroLances.size());
+        assertEquals(600.00, tresMaioresLancesDevolvidosParaQuatroLances.get(0).getValor(), DELTA);
+        assertEquals(500.00, tresMaioresLancesDevolvidosParaQuatroLances.get(1).getValor(), DELTA);
+        assertEquals(400.00, tresMaioresLancesDevolvidosParaQuatroLances.get(2).getValor(), DELTA);
+
+        CONSOLE.propoe(new Lance(ALEX, 700.00));
+
+        final List<Lance> tresMaioresLancesDevolvidosParaCincoLances = CONSOLE.tresMaioresLances();
+
+        assertEquals(3, tresMaioresLancesDevolvidosParaCincoLances.size());
+        assertEquals(700.00, tresMaioresLancesDevolvidosParaCincoLances.get(0).getValor(), DELTA);
+        assertEquals(600.00, tresMaioresLancesDevolvidosParaCincoLances.get(1).getValor(), DELTA);
+        assertEquals(500.00, tresMaioresLancesDevolvidosParaCincoLances.get(2).getValor(), DELTA);
     }
 }
