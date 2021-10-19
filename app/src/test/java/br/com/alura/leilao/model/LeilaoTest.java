d35c7920f793d05e884e3ea181d0630f0f02b4b0
Carlos M Valenca <cvalenca@everis.com>
2021-10-19 09:30:53 -0300

    Utilizando o assertThat

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -1,7 +1,15 @@
 package br.com.alura.leilao.model;
 
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -22,25 +30,25 @@ public class LeilaoTest {
     @Test
     public void getDescricaoQuandoRecebeDescricaoDevolveDescricao() {
         // executar ação esperada
-        String descrcaoDevolvida = CONSOLE.getDescricao();
+        String descricaoDevolvida = CONSOLE.getDescricao();
         // testar resultado esperado
-        assertEquals("Console", descrcaoDevolvida);
+//        assertEquals("Console", descricaoDevolvida);
+        assertThat(descricaoDevolvida, is("Console"));
     }
 
-    // [nome do metodo] + [estado do teste] + [resultado esperado]
-    // [deve]+[resultado esperado]+[estado de teste]
     @Test
-    public void deve_DevolvendoMaiorLance_QuandoRecebeApenasUmLance(){
+    public void deve_DevolvendoMaiorLance_QuandoRecebeApenasUmLance() {
         // verifica se devolve maior lance com apenas um lance
         CONSOLE.propoe(new Lance(ALEX, 200.00));
 
-        double maiorLanceDevolvidoDoConsole = CONSOLE.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
         // O delta pega a diferença entre os valores com ponto flutuante e se ele for maior, significa que os valores são equivalentes.
-        assertEquals(200.00, maiorLanceDevolvidoDoConsole, DELTA);
+//        assertEquals(200.00, maiorLanceDevolvidoDoConsole, DELTA);
+        assertThat(maiorLanceDevolvido, closeTo(200.00, DELTA));
     }
 
     @Test
-    public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente(){
+    public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente() {
         // verifica se devolve maior lance com mais de um lance em ordem crescente
         CONSOLE.propoe(new Lance(ALEX, 100.00));
         CONSOLE.propoe(new Lance(new Usuario("Fran"), 200.00));
@@ -51,7 +59,7 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolvendoMenorLance_QuandoRecebeApenasUmLance(){
+    public void deve_DevolvendoMenorLance_QuandoRecebeApenasUmLance() {
         CONSOLE.propoe(new Lance(ALEX, 200.00));
 
         double menorLanceDevolvido = CONSOLE.getMenorLance();
@@ -59,7 +67,7 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente(){
+    public void deve_DevolveMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente() {
         CONSOLE.propoe(new Lance(ALEX, 100.00));
         CONSOLE.propoe(new Lance(new Usuario("Fran"), 200.00));
 
@@ -69,28 +77,43 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolverTresMaioresLances_QuandoRecebeExatosTresLances(){
+    public void deve_DevolverTresMaioresLances_QuandoRecebeExatosTresLances() {
         CONSOLE.propoe(new Lance(ALEX, 200.00));
-        CONSOLE.propoe(new Lance(new Usuario("Fran"), 300.00));
+        CONSOLE.propoe(new Lance(FRAN, 300.00));
         CONSOLE.propoe(new Lance(ALEX, 400.00));
 
-        List<Lance> tresMaioresLancesDevolvidos =  CONSOLE.tresMaioresLances();
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
 
-        assertEquals(3, tresMaioresLancesDevolvidos.size());
-        assertEquals(400.00, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
-        assertEquals(300.00, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
-        assertEquals(200.00, tresMaioresLancesDevolvidos.get(2).getValor(), DELTA);
+//        assertEquals(3, tresMaioresLancesDevolvidos.size());
+//        assertThat(tresMaioresLancesDevolvidos, hasSize(equalTo(3)));
+//        assertEquals(400.00, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
+//        assertThat(tresMaioresLancesDevolvidos, hasItem(new Lance(ALEX, 400.00)));
+//        assertEquals(300.00, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
+//        assertThat(tresMaioresLancesDevolvidos, hasItem(new Lance(FRAN, 300.00)));
+//        assertEquals(200.00, tresMaioresLancesDevolvidos.get(2).getValor(), DELTA);
+//        assertThat(tresMaioresLancesDevolvidos, hasItem(new Lance(ALEX, 200.00)));
+//
+//        assertThat(tresMaioresLancesDevolvidos, contains(
+//                new Lance(ALEX, 400.00),
+//                new Lance(FRAN, 300.00),
+//                new Lance(ALEX, 200.00)));
+
+        assertThat(tresMaioresLancesDevolvidos,
+                both(Matchers.<Lance>hasSize(3)).and(contains(
+                        new Lance(ALEX, 400.00),
+                        new Lance(FRAN, 300.00),
+                        new Lance(ALEX, 200.00))));
     }
 
     @Test
-    public void deve_DevolverTresMaioresLances_QuandoNaoRecebeLances(){
+    public void deve_DevolverTresMaioresLances_QuandoNaoRecebeLances() {
         List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
 
         assertEquals(0, tresMaioresLancesDevolvidos.size());
     }
 
     @Test
-    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasUmLance(){
+    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasUmLance() {
         CONSOLE.propoe(new Lance(ALEX, 200.00));
 
         List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
@@ -100,7 +123,7 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasDoisLances(){
+    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasDoisLances() {
         CONSOLE.propoe(new Lance(ALEX, 300.00));
         CONSOLE.propoe(new Lance(new Usuario("Fran"), 400.00));
 
@@ -112,7 +135,7 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolverTresLances_QuandoReceberMaisDeTresLances(){
+    public void deve_DevolverTresLances_QuandoReceberMaisDeTresLances() {
         CONSOLE.propoe(new Lance(ALEX, 300.00));
         CONSOLE.propoe(new Lance(FRAN, 400.00));
         CONSOLE.propoe(new Lance(ALEX, 500.00));
@@ -135,35 +158,35 @@ public class LeilaoTest {
     }
 
     @Test
-    public void deve_DevolverValorZeroParaMaiorLance_QuandoNaoTiverLances(){
+    public void deve_DevolverValorZeroParaMaiorLance_QuandoNaoTiverLances() {
         double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(0.00, maiorLanceDevolvido, DELTA);
     }
 
     @Test
-    public void deve_DevolverValorZeroParaMenorLance_QuandoNaoTiverLances(){
+    public void deve_DevolverValorZeroParaMenorLance_QuandoNaoTiverLances() {
         double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(0.0, menorLanceDevolvido, DELTA);
     }
 
     @Test(expected = LanceMenorQueUltimoLanceException.class)
-    public void naoDeve_AdicionarLance_QuandoForMenorQueOMaiorLance(){
+    public void naoDeve_AdicionarLance_QuandoForMenorQueOMaiorLance() {
         CONSOLE.propoe(new Lance(ALEX, 500.00));
         CONSOLE.propoe(new Lance(FRAN, 400.00));
 
     }
 
     @Test(expected = LanceSeguidoDoMesmoUsuarioException.class)
-    public void naoDeve_AdicionarLance_QuandoForOMesmoUsuarioDoUltimoLance(){
+    public void naoDeve_AdicionarLance_QuandoForOMesmoUsuarioDoUltimoLance() {
         CONSOLE.propoe(new Lance(ALEX, 500.00));
         CONSOLE.propoe(new Lance(ALEX, 600.00));
 
     }
 
     @Test(expected = UsuarioJaDeuCincoLancesException.class)
-    public void naoDeve_AdicionarLance_QuandoUsuarioDerCincoLances(){
+    public void naoDeve_AdicionarLance_QuandoUsuarioDerCincoLances() {
         CONSOLE.propoe(new Lance(ALEX, 100.00));
         CONSOLE.propoe(new Lance(FRAN, 200.00));
         CONSOLE.propoe(new Lance(ALEX, 300.00));
